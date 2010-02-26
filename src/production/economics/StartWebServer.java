e8a2749140a7c6dea987ecc23a086ba94ef7005c
unknown <cyrus@.(none)>
2010-02-26 17:56:19 -0500

    finally got the jaxb to work, and got the spring help from matt/ken

---
+++ src/production/economics/StartWebServer.java
@@ -0,0 +1,25 @@
+package economics;
+
+
+public class StartWebServer {
+    private static int PORT = 8080;
+    public static final String APP_PATH = "vinny_testa";
+
+    public static void main(String[] args) {
+//        if (args.length == 1) PORT = Integer.parseInt(args[0]);
+//        Server server = new Server(PORT);
+//        ContextHandlerCollection contexts = new ContextHandlerCollection();
+//        contexts.addHandler(new WebAppContext(server, ".", '/' + APP_PATH));
+//        server.addHandler(contexts);
+//        start(server);
+//    }
+//
+//    private static void start(AbstractLifeCycle server) {
+//        try {
+//            server.start();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+    }
+}
